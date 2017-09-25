4b68be197b3c108a1d051e0c0ac40cb011b8aa78
Manuel Leuenberger <leuenberger@inf.unibe.ch>
2017-09-25 15:28:48 +0200

    rename and license

--- src/main/java/ch/unibe/scg/ese2017/fullintegration/Application.java
+++ src/main/java/ch/unibe/scg/ese2017/fullintegration/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ese2017FullIntegrationApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ese2017FullIntegrationApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
