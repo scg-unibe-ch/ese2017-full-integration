83b4441ba3bc09a113dc17e6cdd390133f6c5a07
Manuel Leuenberger <leuenberger@inf.unibe.ch>
2017-09-25 15:25:48 +0200

    initialize spring boot project

---
+++ src/main/java/ch/unibe/scg/ese2017/fullintegration/Ese2017FullIntegrationApplication.java
@@ -0,0 +1,12 @@
+package ch.unibe.scg.ese2017.fullintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ese2017FullIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ese2017FullIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/ch/unibe/scg/ese2017/fullintegration/Ese2017FullIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.unibe.scg.ese2017.fullintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ese2017FullIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
